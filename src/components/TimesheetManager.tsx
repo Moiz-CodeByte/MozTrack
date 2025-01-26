df82df7f0028f3236e4a4a7ed3c9afd0dec6b76f
MoizCodeByte <hello@abdulmoiz.net>
2025-01-26 19:16:45 +0500

    updated

--- src/components/TimesheetManager.tsx
+++ src/components/TimesheetManager.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const TimesheetManager = () => {
   const [timesheets, setTimesheets] = useState([]);
